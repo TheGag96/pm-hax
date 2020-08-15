08721ab9097cbc47adcce6e422e0bb8945f3ef78
TheGag96 <thegag96@gmail.com>
2020-08-15 01:59:44 -0500

    Add "PSA Conversion Tools" folder, along with the Fighter.pac Extractor tool

---
+++ PSA Conversion Tools/README.md
@@ -0,0 +1,30 @@
+# PSA Conversion Tools
+
+This folder contains tools that can be used to extract/manipulate PSA code.
+
+## Fighter.pac Extractor
+
+Written by codes. This is a command-line tool that is able to extract the entirety of Fighter.pac, including all action entry and exit routines and the subroutines they call, and outputting them in a format you can copy and paste into PSA Compressor. It does this by reading from a RAM dump, meaning that the results of global Fighter.pac code edits made by PM/P+ can be naturally read. (This is especially helpful for implementing Action Overrides on a moveset, since now you can copy-paste the original and then simply modify, not worrying whether you're leaving out important normal functionality.)
+
+To use, you'll need to get a RAM dump boot the game up in Dolphin, far enough in to where you know all Fighter.pac is loaded and the Gecko codes for it are applied (just get to the CSS or in game). Then go to View -> Memory and click "Dump MRAM". In your Dolphin directory, go to `User/Dump`, and copy the resulting file into the same directory as `fighterextract.exe` as `ram.raw`. Then, run on the command line with:
+
+```bat
+fighterextract.exe > extracted.txt
+```
+
+Example output:
+
+```
+Action 0 (Entry) @ 80FC1718:
+  E=00070100:0-80FC16E0,E=00070100:0-80FAB894,
+
+Action 1 (Entry) @ 80FABD5C:
+  E=120A0100:5-22000010,E=02010200:0-0000000E,6-00000004,E=00070100:0-80FAB68C,E=020A0100:0-00000001,E=020A0100:0-00000002,E=020A0100:0-00000003,E=020A0100:0-00000004,E=020A0100:0-00000005,E=020A0100:0-00000006,E=020A0100:0-00000007,E=020A0100:0-00000008,E=02090200:0-00000008,0-0000273E,E=02090200:0-00000008,0-0000274B,E=02010500:0-00000002,6-00000007,5-000003F5,0-00000000,5-00000C2D,E=02040400:6-00000007,5-00000027,0-00000000,1-0000EA60,E=02040100:6-00000003,E=02010500:0-00000002,6-00000007,5-000003F3,0-00000000,1-00000000,E=02040400:6-00000007,5-00000027,0-00000000,1-0000EA60,E=02040100:6-00000003,E=02010500:0-00000000,6-00000007,5-000003F5,0-00000000,5-00000C2D,E=02040100:6-00000003,E=02010500:0-00000000,6-00000007,5-000003F3,0-00000000,1-00000000,E=02040100:6-00000003,E=02000400:0-0000271A,0-0000008C,6-00002714,0-00000003,E=02040100:6-00000003,E=02080100:0-0000271A,E=0D000200:0-00000009,0-80FABB4C,
+
+Action 2 (Entry) @ 80FABE6C:
+  E=00070100:0-80FC16E0,E=02010200:0-00000000,6-00000001,E=04000300:0-0000000D,3-00000000,1-00000000,
+
+(...)
+```
+
+This program was written in [D](http://dlang.org).
---
+++ PSA Conversion Tools/fighterextract.d
@@ -0,0 +1,119 @@
+import std.stdio, std.file, std.process, std.algorithm, std.conv, std.string, std.bitmanip, std.container, std.range, std.regex, std.format;
+
+struct CodePiece {
+  string type;
+  uint location;
+}
+
+enum FIGHTER_PAC = 0x80F9FC00;
+enum FIGHTER_PAC_WORD_OFFSET = (FIGHTER_PAC-0x80000000u)/4;
+
+void main(string[] args) {
+  uint entryList = 0x2038C;
+  uint exitList  = 0x207D4;
+  auto app = appender!string;
+  bool[uint] processedTable;
+
+  //unloads RAM dump into array of 4-byte words (converted from big endian)
+  uint[] dolphinDump = loadDump();
+
+  CodePiece[] actions;
+  actions.reserve(0x112*2);
+
+  foreach (i; 0..0x112) {
+    auto entryOffset = dolphinDump[FIGHTER_PAC_WORD_OFFSET + i + entryList/4];
+
+    if (entryOffset) {
+      actions ~= CodePiece(
+        format("Action %X (Entry)", i),
+        entryOffset
+      );
+    }
+
+    auto exitOffset = dolphinDump[FIGHTER_PAC_WORD_OFFSET + i + exitList/4];
+
+    if (exitOffset) {
+      actions ~= CodePiece(
+        format("Action %X (Exit)", i),
+        exitOffset
+      );
+    }
+  }
+
+  auto queue = DList!CodePiece(actions);
+
+  while (!queue.empty) {
+    auto front = queue.front;
+    queue.removeFront;
+
+    writefln("%s @ %08X:", front.type, front.location);
+
+    char[] result;
+
+    if (front.location < 0x80000000) {
+      result = "This address is invalid.".dup;
+    }
+    else {
+      result = readPSA(front.location, dolphinDump);
+    }
+
+    static immutable re = ctRegex!`2-([89][0-9A-F]{7})`;
+
+    auto matches = matchAll(result, re);
+
+    foreach (match; matches) {
+      auto subAddr = match[1].to!uint(16);
+
+      //Change pointer args (2) to value args (0) so that you can call subroutines at absolute addresses properly. The
+      //game had already converted the once-relative addresses to absolute ones when Fighter.pac loaded, and so if you
+      //were to copy-paste the output of this script into a PSA and tried to run it in-game, it'd try to convert them
+      //into absolute addresses AGAIN...
+      match[0][0] = '0';
+
+      if (subAddr !in processedTable) {
+        queue.insertBack(CodePiece("Sub", subAddr));
+        processedTable[subAddr] = true;
+      }
+    }
+
+    writeln("  ", result, "\n");
+  }
+}
+
+uint[] loadDump() {
+  ubyte[] raw    = cast(ubyte[]) read("ram.raw");
+  uint[]  result = new uint[](raw.length/4);
+
+  foreach (i; 0..result.length) {
+    result[i] = raw[4*i] << 24 | raw[4*i+1] << 16 | raw[4*i+2] << 8 | raw[4*i+3];
+  }
+
+  return result;
+}
+
+uint readMem(uint addr, uint[] dolphinDump) {
+  uint index = (addr-0x80000000u)/4;
+  return dolphinDump[index];
+}
+
+char[] readPSA(uint addr, uint[] dolphinDump) {
+  auto app     = appender!(char[]);
+  uint curAddr = addr;
+
+  while (true) {
+    uint eventCode = readMem(curAddr, dolphinDump);
+    if (eventCode == 0x0 || eventCode == 0x00080000) break;
+
+    uint argAddr   = readMem(curAddr+4, dolphinDump);
+
+    app.formattedWrite("E=%08X:", eventCode);
+
+    foreach (a; 0..(eventCode >> 8) & 0xFF) {
+      app.formattedWrite("%d-%08X,", readMem(argAddr+a*8, dolphinDump), readMem(argAddr+a*8+4, dolphinDump));
+    }
+
+    curAddr += 8;
+  }
+
+  return app.data;
+}
